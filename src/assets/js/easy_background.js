a7d028cf72a65eecda190ac7bf4532abb4cdac6c
lucapette <ciao@lucapette.me>
2023-08-23 11:29:15 +0200

    minor changes plus formatting

--- src/assets/js/easy_background.js
+++ src/assets/js/easy_background.js
@@ -8,7 +8,6 @@
  * --------------------------------------------------------------------------*/
 
 function easy_background(selector, sld_args) {
-
   function empty_img(x) {
     if (x) {
       return "<img src='" + x + "'>";
@@ -30,42 +29,46 @@ function easy_background(selector, sld_args) {
   document.body.insertBefore(p, document.body.firstChild);
   //switch all values in object -- objectname.index in you case sld_args is object and i is index of array which keep images (i). We use this function for fill div with img tags
   //and for insert delays into empty or forgotten places in object
-  sld_args.slide.forEach(function(v, i) {
+  sld_args.slide.forEach(function (v, i) {
     if (v) {
       document.querySelector(".easy_slider").innerHTML += empty_img(v);
-      if (typeof sld_args.delay[i] == 'undefined' || typeof sld_args.delay[i] == '' || sld_args.delay[i] == 0) {
+      if (
+        typeof sld_args.delay[i] == "undefined" ||
+        typeof sld_args.delay[i] == "" ||
+        sld_args.delay[i] == 0
+      ) {
         sld_args.delay[i] = def_del;
       }
     }
-
   });
 
   //add various style on selector
   document.querySelector(".easy_slider").style.display = "none";
-
 
   //add various style on selector
   document.querySelector(selector).style.backgroundSize = "cover";
   document.querySelector(selector).style.backgroundRepeat = "no-repeat";
   document.querySelector(selector).style.backgroundPosition = "center center";
 
-
-  setTimeout(function() {
+  setTimeout(function () {
     //add various style on selector
-    if (typeof sld_args.transition_timing === 'undefined') {
+    if (typeof sld_args.transition_timing === "undefined") {
       sld_args.transition_timing = "ease-in";
     }
-    if (typeof sld_args.transition_duration === 'undefined') {
+    if (typeof sld_args.transition_duration === "undefined") {
       sld_args.transition_duration = 500;
     }
-    var transition = "all " + sld_args.transition_duration + 'ms ' + sld_args.transition_timing;
+    var transition =
+      "all " +
+      sld_args.transition_duration +
+      "ms " +
+      sld_args.transition_timing;
     document.querySelector(selector).style.WebkitTransition = transition;
     document.querySelector(selector).style.MozTransition = transition;
     document.querySelector(selector).style.MsTransition = transition;
     document.querySelector(selector).style.OTransition = transition;
     document.querySelector(selector).style.transition = transition;
   }, 100);
-
 
   //this n is number of row  in object - if first row one function if more than 1 then other
   var n = 1;
@@ -74,40 +77,37 @@ function easy_background(selector, sld_args) {
 
   function slider() {
     //switching all images one by one
-    sld_args.slide.forEach(function(vvv, iii) {
+    sld_args.slide.forEach(function (vvv, iii) {
       //here go all slides except first
       if (n > 1) {
         //set delay from collected number from previous slides
         var delay = li;
-        setTimeout(function() {
-
-          document.querySelector(selector).style.backgroundImage = "url('" + vvv + "')";
-
+        setTimeout(function () {
+          document.querySelector(selector).style.backgroundImage =
+            "url('" + vvv + "')";
         }, delay); // >1
         //collecting delays from curent
         li = li + sld_args.delay[iii];
-
-      } else { //this function for only  first slide
+      } else {
+        //this function for only  first slide
 
         //next row
         n++;
         //collect delay first time
         li = sld_args.delay[iii];
 
-        document.querySelector(selector).style.backgroundImage = "url('" + vvv + "')";
-
+        document.querySelector(selector).style.backgroundImage =
+          "url('" + vvv + "')";
       }
-
     });
-
-  };
+  }
 
   slider();
 
-  setInterval(function() { // REPEAT
+  setInterval(function () {
+    // REPEAT
 
     slider();
     //here used length of array of delays in object instead you tot_time variable
   }, sld_args.delay.length);
-
 }
